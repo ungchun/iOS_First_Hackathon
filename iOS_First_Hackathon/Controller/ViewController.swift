9d663b1697ae412bd361beb4cc7c2c0e7380f4d8
ungchun <leedool3003@naver.com>
2022-10-05 14:21:10 +0900

    test : test gitignore

--- iOS_First_Hackathon/Controller/ViewController.swift
+++ iOS_First_Hackathon/Controller/ViewController.swift
@@ -5,6 +5,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return }
+
+        
+        print("KEY = \(apiKey)")
     }
 }
-
